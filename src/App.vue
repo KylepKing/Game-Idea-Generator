5a2d20a0ecd9f9b344c2d664b136ddf2ed132a6b
Kyle <kyle.p.king1@gmail.com>
2025-07-03 13:14:03 +0100

    Init of godot game maker assistant

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyADj2B2tgbg8jJjyPcpCSdLgxLEyExIC_0',
+  authDomain: 'godot4-gamemaker-assistant.firebaseapp.com',
+  projectId: 'godot4-gamemaker-assistant',
+  storageBucket: 'godot4-gamemaker-assistant.firebasestorage.app',
+  messagingSenderId: '705783289749',
+  appId: '1:705783289749:web:36e59d03d1e9c54eccc89c',
+  measurementId: 'G-P5Z0WYFZWQ',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
